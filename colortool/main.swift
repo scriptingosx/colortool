696101fc54bcc1504f534e519337d235157521ea
Armin Briegel <ab@scriptingosx.com>
2019-12-18 09:19:34 +0100

    Initial Commit

---
+++ colortool/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  colortool
+//
+//  Created by Armin Briegel on 18.12.19.
+//  Copyright © 2019 Scripting OS X. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+

b96467ce4b1e099cf3bd240f40d3102b58ae7a07
Armin Briegel <ab@scriptingosx.com>
2019-12-18 10:49:37 +0100

    first implementation

--- colortool/main.swift
+++ colortool/main.swift
@@ -6,7 +6,39 @@
 //  Copyright © 2019 Scripting OS X. All rights reserved.
 //
 
-import Foundation
+import Cocoa
 
-print("Hello, World!")
+func colorImage(_ color: NSColor) -> NSImage {
+  let size = NSSize(width: 32, height: 32)
+  let rect = NSRect(x: 0, y: 0, width: 32, height: 32)
+  let image = NSImage()
+  image.size = size
+  image.lockFocus()
+  color.drawSwatch(in: rect)
+  image.unlockFocus()
+  return image
+}
+
+func write(image: NSImage, toURL url: URL) {
+  if let imageRep = NSBitmapImageRep(data: image.tiffRepresentation!) {
+    if let data = imageRep.representation(using: .png, properties: [:]) {
+      do {
+        try data.write(to: url)
+      } catch {
+        print("could not write to \(url.path)")
+        exit(1)
+      }
+    }
+  }
+
+}
+
+let fileurl = URL(fileURLWithPath: "/Users/armin/Desktop/color.png")
+
+let color = NSColor(calibratedRed: 1.0, green: 1.0, blue: 0.0, alpha: 1.0)
+
+let image = colorImage(color)
+write(image: image, toURL: fileurl)
+
+
 
